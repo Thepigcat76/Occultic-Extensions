dc0a7a7e37bc734f7870b1d986cd1bb2ee3616db
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-02-01 17:33:46 +0100

    test

--- src/main/java/net/thepigcat76/occultic_extensions/registry/ModItems.java
+++ src/main/java/net/thepigcat76/occultic_extensions/registry/ModItems.java
@@ -1,6 +1,7 @@
 package net.thepigcat76.occultic_extensions.registry;
 
-import net.minecraft.world.item.CreativeModeTab;
+import com.github.klikli_dev.occultism.Occultism;
+import com.github.klikli_dev.occultism.common.OccultismCreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,7 +14,7 @@ public class ModItems {
     public static final DeferredRegister<Item> MOD_ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
     public static final RegistryObject<Item> SILVER_SCEPTER = MOD_ITEMS.register("silver_scepter",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+            () -> new Item(new Item.Properties().tab(Occultism.ITEM_GROUP)));
 
     public static void register(IEventBus eventBus) {
         MOD_ITEMS.register(eventBus);
