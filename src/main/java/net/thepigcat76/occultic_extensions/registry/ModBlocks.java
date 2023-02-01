69eb637c5e6ab97f6290e40677de0cca05807e94
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-02-01 16:30:47 +0100

    Dependencies are set up!

--- src/main/java/net/thepigcat76/occultic_extensions/registry/ModBlocks.java
+++
@@ -1,4 +0,0 @@
-package net.thepigcat76.occultic_extensions.registry;
-
-public class ModBlocks {
-}
--- src/main/java/net/thepigcat76/occultic_extensions/registry/ModItems.java
+++ src/main/java/net/thepigcat76/occultic_extensions/registry/ModItems.java
@@ -1,4 +1,21 @@
 package net.thepigcat76.occultic_extensions.registry;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import static net.thepigcat76.occultic_extensions.Occultic_extensions.MODID;
+
 public class ModItems {
+    public static final DeferredRegister<Item> MOD_ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    public static final RegistryObject<Item> SILVER_SCEPTER = MOD_ITEMS.register("silver_scepter",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+
+    public static void register(IEventBus eventBus) {
+        MOD_ITEMS.register(eventBus);
+    }
 }
